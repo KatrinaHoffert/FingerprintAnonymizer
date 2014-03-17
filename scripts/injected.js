cf75a44629dc4e29616344399a2bbc69ac7d83bf
Kat Hoffert <kat.hoffert@gmail.com>
2014-03-17 14:13:03 -0600

    Added assignment and file from meeting.

--- scripts/injected.js
+++ scripts/injected.js
@@ -1,2 +1,8 @@
 console.log("Injected script is definitely running! The page is " +
 	window.location.href);
+
+// Prevent access to plugin info
+// TODO: Backup window.navigator
+window.navigator = null;
+window.navigator = {};
+// TODO: Recreate window.navigator
