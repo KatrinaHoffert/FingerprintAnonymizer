86d27c1ac157d5f7c5183285e8ce1a01cde2cede
Kat Hoffert <kat.hoffert@gmail.com>
2014-03-17 23:54:49 -0600

    Basic settings and whitelist implemented.
    The whitelist is a newline delimited text area that stores regex
    strings. JQuery is now included in the project (because it's not
    JavaScript without JQuery!).

---
+++ scripts/settings.js
@@ -0,0 +1,34 @@
+chrome.storage.sync.get(
+	'whitelist',
+	function (result){
+		// Get the stored whitelist array
+		var whitelist = result.whitelist;
+
+		// Iterate over array and populate our page
+		for(var i = 0; i < whitelist.length; i++)
+		{
+			$('#whitelist').append(whitelist[i] + "\n");
+		}
+	}
+);
+
+// Run when the save button is clicked
+$('#save').click(function(){
+	var arrayByLine = $('#whitelist').val().split('\n');
+
+	// Remove blanks
+	// From: http://stackoverflow.com/a/2843625/1968462
+	arrayByLine = $.grep(arrayByLine, function(n){
+			return(n);
+	});
+
+	// Save the settings
+	chrome.storage.sync.set(
+		{
+			'whitelist': arrayByLine
+		},
+		function(){
+			alert("Your settings have been saved");
+		}
+	);
+});
