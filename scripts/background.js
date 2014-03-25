d5732d2ca64cd5476306fecb5b52cc28ccf2e6e6
Kat Hoffert <kat.hoffert@gmail.com>
2014-03-24 22:57:10 -0600

    HTTP headers now only changed for non-whitelisted.
    However, this means that the background script is reloading the
    whitelist every second. Possibly inefficient? Also, whitelist
    changes take a second to be reloaded into the background script
    (and thus not applied to the HTTP headers for 1s after making the
    change). I think it's a reasonable trade off.
    
    Technical reasons are that the function for loading the whitelist
    is asynchronous and if we directly call it in the action listener,
    it won't complete until (likely) after the action listener has
    already returned (at which point of time it's too late). Thus,
    the whitelist is concurrently updated in the background, independent
    from the action listener. This actually might be faster, anyway,
    since many sites have a very large number of requests and it's not
    ideal to be reloading the whitelist a thousand times per second.

--- scripts/background.js
+++ scripts/background.js
@@ -1,16 +1,50 @@
-//We want to react before the headers are sent, so that they can be modified.
+/**
+ * Helper function for retrieving the domain of a url.
+ * Created by Filip Roseen - refp
+ * http://stackoverflow.com/a/8498668/1968462
+ */
+function urlDomain(data) {
+	var a = document.createElement('a');
+	a.href = data;
+	return a.hostname;
+}
+
+var whitelist = [];
+
+// We want to react before the headers are sent, so that they can be modified.
 chrome.webRequest.onBeforeSendHeaders.addListener(
 	function(details)
 	{
-		for(var i = 0; i < details.requestHeaders.length; ++i)
+		// Figure out the domain we're about to access
+		var domain = urlDomain(details.url);
+
+		// Iterate over array and check for matches with our domain
+		var found = false;
+		for(var i = 0; i < whitelist.length; i++)
 		{
-			//Set language header to something more generic
-			// Accepts US English, or any other English
-			if(details.requestHeaders[i].name === 'Accept-Language')
+			// Check if domain is on the whitelist
+			if(domain.indexOf(whitelist[i]) !== -1)
 			{
-				details.requestHeaders[i].value = "en-US,en;q=0.8";
+				found = true;
+				break;
 			}
 		}
+
+		// Only change header if site not in whitelist
+		if(!found)
+		{
+			// Otherwise find the correct header to change
+			for(var i = 0; i < details.requestHeaders.length; i++)
+			{
+				//Set language header to something more generic
+				if(details.requestHeaders[i].name === 'Accept-Language')
+				{
+					details.requestHeaders[i].value = "en-US,en;q=0.8";
+					break;
+				}
+			}
+		}
+
 		return {requestHeaders: details.requestHeaders};
 	},
 	{
@@ -33,3 +67,26 @@ chrome.webRequest.onBeforeRequest.addListener(
 	},
 	["blocking"]
 );
+
+/**
+ * Function for reloading the whitelist ever 1000 ms. Possibly inefficient (?).
+ * Ideally (?) we'd have to load the whitelist on each page request, but loading
+ * the whitelist is asynchronous and can't be done inside our event handler
+ * (I think?). On the bright side, this is more efficient if there's a large
+ * number of page requests at a time (which there often is, since every image,
+ * script, etc, is a new request). As a result, I'm not sure if this is more or
+ * less efficient than reloading the list for each request.
+ */
+setInterval(function(){
+	chrome.storage.sync.get(
+		'whitelist',
+		function (result){
+			// Get the stored whitelist array
+			whitelist = result.whitelist;
+
+			if(whitelist === undefined) whitelist = [];
+		}
+	);
+
+	console.log(JSON.stringify(whitelist))
+}, 1000);
